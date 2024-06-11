85940dd080d318e5359ade360ba153785c163d8a
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-06-11 16:32:02 +0530

    Update __init__.py

--- Pbxbot/core/__init__.py
+++ Pbxbot/core/__init__.py
@@ -1,7 +1,7 @@
 from .clients import Pbxbot
 from .config import ENV, Config, Limits, Symbols
 from .database import db
-from .initializer import ForcesubSetup, GachaBotsSetup, TemplateSetup, UserSetup
+from .initializer import GachaBotsSetup, TemplateSetup, UserSetup
 from .logger import LOGS
 
 __all__ = [
