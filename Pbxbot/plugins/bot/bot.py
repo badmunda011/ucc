3a0b7e5e3056a63a4105185b0406e0cffb47f5eb
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-07-14 19:33:23 +0530

    Update bot.py

--- Pbxbot/plugins/bot/bot.py
+++ Pbxbot/plugins/bot/bot.py
@@ -10,8 +10,18 @@ from ..btnsG import gen_bot_help_buttons, start_button
 from . import HELP_MSG, START_MSG, BotHelp, Config, Pbxbot
 
 
+@Pbxbot.bot.on_message(filters.command("start"))
+async def start_pm(_, message: Message):
+    btns = start_button()
 
-@Pbxbot.bot.on_message(filters.command("help"))
+    await message.reply_text(
+        START_MSG.format(message.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(btns),
+    )
+
+
+@Pbxbot.bot.on_message(filters.command("help") & Config.AUTH_USERS)
 async def help_pm(_, message: Message):
     btns = gen_bot_help_buttons()
 
@@ -24,7 +34,7 @@ async def help_pm(_, message: Message):
 
 @Pbxbot.bot.on_message(filters.command("restart") & Config.AUTH_USERS)
 async def restart_clients(_, message: Message):
-    await message.reply_text("Rᴇsᴛᴀʀᴛᴇᴅ Bᴏᴛ Sᴜᴄᴄᴇssғᴜʟʟʏ ᴘʙx 2.0  ✅")
+    await message.reply_text("Restarted Bot Successfully ✅")
     try:
         if HEROKU_APP:
             try:
@@ -40,6 +50,8 @@ async def restart_clients(_, message: Message):
 
 
 BotHelp("Others").add(
+    "start", "To start the bot and get the main menu."
+).add(
     "help", "To get the help menu with all the command for this assistant bot."
 ).add(
     "restart", "To restart the bot."
