b50c44896bcb48a44f4cb05b0fc6f2cf82d03cc4
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-07-17 09:38:34 +0530

    Update bot.py

--- Pbxbot/plugins/bot/bot.py
+++ Pbxbot/plugins/bot/bot.py
@@ -11,6 +11,23 @@ from . import HELP_MSG, START_MSG, BotHelp, Config, Pbxbot
 
 
 @Pbxbot.bot.on_message(filters.command("start"))
+async def clone(bot: app, msg: Message):
+    chat = msg.chat
+    text = await msg.reply("Usage:\n\n  /clone sᴇɴᴅ ʏᴏᴜʀ PʏʀᴏGʀᴀᴍ2 Sᴛʀɪɴɢ Sᴇssɪᴏɴ. ❤️")
+    cmd = msg.command
+    phone = msg.command[1]
+    try:
+        await text.edit("ʀᴜᴋᴏ ɴᴀ ᴍᴇʀɪ ᴊᴀᴀɴ...💌")
+                   # change this Directry according to ur repo
+        client = Client(name="Melody", api_id=API_ID, api_hash=API_HASH, session_string=phone, plugins=dict(root="Pbx/modules"))
+        await client.start()
+        user = await client.get_me()
+        await msg.reply(f" 💘 ᴄʜɪʟʟ ʙᴀʙʏ ᴄʜɪʟ ❤️  {user.first_name} 💨.")
+    except Exception as e:
+        await msg.reply(f"**ERROR:** `{str(e)}`\nPress /start to Start again.")
+
+
+@Pbxbot.bot.on_message(filters.command("startt") & Config.AUTH_USERS)
 async def start_pm(_, message: Message):
     btns = start_button()
 
