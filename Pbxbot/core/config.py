c561d2ceb792742e4aa23078753f839a3f4db843
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-07-25 10:31:11 +0530

    Update config.py

--- Pbxbot/core/config.py
+++ Pbxbot/core/config.py
@@ -163,3 +163,9 @@ contact_filter = filters.create(
     lambda _, __, message: (message.from_user and message.from_user.is_contact)
     or message.outgoing
 )
+if GROUP_MODE == ("True" or "true"):
+    grp = True
+else:
+    grp = False
+
+GRPPLAY = grp
