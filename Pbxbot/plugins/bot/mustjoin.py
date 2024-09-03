8abbd2b2aafc3237a6e711b54c875ad57abbcc39
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-09-03 23:10:06 +0530

    Update mustjoin.py

--- Pbxbot/plugins/bot/mustjoin.py
+++ Pbxbot/plugins/bot/mustjoin.py
@@ -17,36 +17,29 @@ async def must_join_channel(app: Client, msg: Message):
         return
     try:
         try:
-            await app.get_chat_member(MUST_JOIN,MUST_JOIN2,MUST_JOIN3,MUST_JOIN4, msg.from_user.id)
+            await app.get_chat_member(MUST_JOIN, msg.from_user.id)
         except UserNotParticipant:
             if MUST_JOIN.isalpha():
                 link = "https://t.me/" + MUST_JOIN
-                link2 = "https://t.me/" + MUST_JOIN2
-                link3 = "https://t.me/" + MUST_JOIN3
-                link4 = "https://t.me/" + MUST_JOIN4
-                
             else:
-                chat_info = await app.get_chat(MUST_JOIN,MUST_JOIN2,MUST_JOIN3,MUST_JOIN4)
+                chat_info = await app.get_chat(MUST_JOIN)
                 link = chat_info.invite_link
-                link2 = chat_info.invite_link
-                link3 = chat_info.invite_link
-                link4 = chat_info.invite_link
             try:
                 await msg.reply_photo(
-                    photo="https://telegra.ph/file/afdb310aefd322f49de79.jpg", caption=f"๏ ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ᴍʏ ᴅᴀᴛᴀʙᴀsᴇ ʏᴏᴜ'ᴠᴇ ɴᴏᴛ ᴊᴏɪɴᴇᴅ , ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴜsᴇ ᴍᴇ ᴛʜᴇɴ ᴊᴏɪɴ  ᴀɴᴅ sᴛᴀʀᴛ ᴍᴇ ᴀɢᴀɪɴ ! ",
+                    photo="https://telegra.ph/file/d44e57c7d8586edf6ee18.jpg", caption=f"๏ ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ᴍʏ ᴅᴀᴛᴀʙᴀsᴇ ʏᴏᴜ'ᴠᴇ ɴᴏᴛ ᴊᴏɪɴᴇᴅ [๏sᴜᴘᴘᴏʀᴛ๏]({link}) ʏᴇᴛ, ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴜsᴇ ᴍᴇ ᴛʜᴇɴ ᴊᴏɪɴ [๏sᴜᴘᴘᴏʀᴛ๏]({link}) ᴀɴᴅ sᴛᴀʀᴛ ᴍᴇ ᴀɢᴀɪɴ ! ",
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("๏Jᴏɪɴº๏", url=link),
+                                InlineKeyboardButton("๏Jᴏɪɴº๏", url=f"https://t.me/ll_THE_BAD_BOT_ll"),
                             ],
                             [
-                            InlineKeyboardButton("๏Jᴏɪɴ¹๏", url=link2),
+                            InlineKeyboardButton("๏Jᴏɪɴ¹๏", url=f"https://t.me/ll_BAD_MUNDA_WORLD_ll"),
                             ],
                             [
-                            InlineKeyboardButton("๏Jᴏɪɴ²๏", url=link3),
+                            InlineKeyboardButton("๏Jᴏɪɴ²๏", url=link),
                             ],
                             [
-                            InlineKeyboardButton("๏Jᴏɪɴ³๏", url=link4),
+                            InlineKeyboardButton("๏Jᴏɪɴ³๏", url=f"https://t.me/HEROKUBIN_01")
                             ]
                         ]
                     )
