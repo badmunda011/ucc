c521e47cc6c0a41cb6a70e04abf67b84ccf1a796
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-09-03 22:50:18 +0530

    Update mustjoin.py

--- Pbxbot/plugins/bot/mustjoin.py
+++ Pbxbot/plugins/bot/mustjoin.py
@@ -6,6 +6,10 @@ from . import Pbxbot
 #--------------------------
 
 MUST_JOIN = "PBX_CHAT"
+MUST_JOIN2 = "HEROKUBIN_01"
+MUST_JOIN3 = "ll_THE_BAD_BOT_ll"
+MUST_JOIN4 = "ll_BAD_MUNDA_WORLD_ll"
+
 #------------------------
 @Pbxbot.bot.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
@@ -17,16 +21,29 @@ async def must_join_channel(app: Client, msg: Message):
         except UserNotParticipant:
             if MUST_JOIN.isalpha():
                 link = "https://t.me/" + MUST_JOIN
+                link2 = "https://t.me/" + MUST_JOIN2
+                link3 = "https://t.me/" + MUST_JOIN3
+                link4 = "https://t.me/" + MUST_JOIN4
+                
             else:
                 chat_info = await app.get_chat(MUST_JOIN)
                 link = chat_info.invite_link
             try:
                 await msg.reply_photo(
-                    photo="https://telegra.ph/file/afdb310aefd322f49de79.jpg", caption=f"๏ ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ᴍʏ ᴅᴀᴛᴀʙᴀsᴇ ʏᴏᴜ'ᴠᴇ ɴᴏᴛ ᴊᴏɪɴᴇᴅ [๏sᴜᴘᴘᴏʀᴛ๏]({link}) ʏᴇᴛ, ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴜsᴇ ᴍᴇ ᴛʜᴇɴ ᴊᴏɪɴ [๏sᴜᴘᴘᴏʀᴛ๏]({link}) ᴀɴᴅ sᴛᴀʀᴛ ᴍᴇ ᴀɢᴀɪɴ ! ",
+                    photo="https://telegra.ph/file/afdb310aefd322f49de79.jpg", caption=f"๏ ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ᴍʏ ᴅᴀᴛᴀʙᴀsᴇ ʏᴏᴜ'ᴠᴇ ɴᴏᴛ ᴊᴏɪɴᴇᴅ , ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴜsᴇ ᴍᴇ ᴛʜᴇɴ ᴊᴏɪɴ  ᴀɴᴅ sᴛᴀʀᴛ ᴍᴇ ᴀɢᴀɪɴ ! ",
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("๏Jᴏɪɴ๏", url=link),
+                                InlineKeyboardButton("๏Jᴏɪɴº๏", url=link),
+                            ],
+                            [
+                            InlineKeyboardButton("๏Jᴏɪɴ¹๏", url=link2),
+                            ],
+                            [
+                            InlineKeyboardButton("๏Jᴏɪɴ²๏", url=link3),
+                            ],
+                            [
+                            InlineKeyboardButton("๏Jᴏɪɴ³๏", url=link4),
                             ]
                         ]
                     )
