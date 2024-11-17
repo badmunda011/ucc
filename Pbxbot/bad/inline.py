f2fda502d734a5ce7e430848f7b012b65e3fe722
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-11-17 15:09:58 +0530

    Delete Pbxbot/bad/inline.py

--- Pbxbot/bad/inline.py
+++
@@ -1,8 +0,0 @@
-from pyrogram.types import InlineKeyboardButton
-
-CHATBOT_ON = [
-    [
-        InlineKeyboardButton(text="ᴇɴᴀʙʟᴇ", callback_data=f"addchat"),
-        InlineKeyboardButton(text="ᴅɪsᴀʙʟᴇ", callback_data=f"rmchat"),
-    ],
-]
