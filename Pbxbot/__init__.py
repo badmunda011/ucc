d4cd05bc7aedbcea3e3acd9f16ef1b37b79ec37f
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-11-17 20:07:34 +0530

    Update __init__.py

--- Pbxbot/__init__.py
+++ Pbxbot/__init__.py
@@ -17,12 +17,8 @@ __version__ = {
     "python": python_version(),
 }
 
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
-LOGS = logging.getLogger(__name__)
-boot = time.time()
 mongo = MongoCli(bad.SUKH)
 db = mongo.Anonymous
-OWNER = bad.OWNER_ID
 
 try:
     if Config.HEROKU_APIKEY is not None and Config.HEROKU_APPNAME is not None:
