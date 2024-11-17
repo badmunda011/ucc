565ea66f2dcd49a8453af9a30332d99a4aaff885
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-11-17 19:46:54 +0530

    Update bad.py

--- bad.py
+++ bad.py
@@ -6,4 +6,6 @@ from pyrogram import filters
 
 load_dotenv()
 
+OWNER_ID = int(getenv("OWNER_ID", "7009601543"))
+
 SUKH = getenv("SUKH", "mongodb+srv://Badmunda_13:badmunda50@cluster0.9oyzqux.mongodb.net/?retryWrites=true&w=majority")
