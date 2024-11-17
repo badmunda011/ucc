28b7f74847f3cd03152e848b4b10c65d2d191495
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-11-17 23:11:56 +0530

    Update Commands.py

--- Pbxbot/plugins/user/Commands.py
+++ Pbxbot/plugins/user/Commands.py
@@ -15,9 +15,9 @@ from . import *
 
 translator = GoogleTranslator()
 
+lang_db = db.ChatLangDb.LangCollection
 status_db = db.chatbot_status_db.status
 
-    
 @on_message("status", allow_stan=True)
 async def status_command(client: Client, message: Message):
     chat_id = message.chat.id
