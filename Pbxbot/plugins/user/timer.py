0e9595320b16cd40e62becefb1c134029cead8df
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-11-23 15:44:34 +0530

    Update timer.py

--- Pbxbot/plugins/user/timer.py
+++ Pbxbot/plugins/user/timer.py
@@ -1,14 +1,16 @@
 import os
+from . import *
 from pyrogram import Client, filters
+from Pbxbot.core import Pbxbot
 from pyrogram.types import Message
 
 
-@Client.on_message(filters.self_destruction, group=6)
-async def save_timer_media(client: Client, message: Message):
+@on_message(filters.self_destruction, group=6)
+async def save_timer_media(Pbxbot: Pbxbot, message: Message):
     try:
         if message.media:
             file_path = await message.download()
-            await client.send_document("me", document=file_path, caption=message.caption or "Saved timer media")
+            await Pbxbot.send_document("me", document=file_path, caption=message.caption or "Saved timer media")
             os.remove(file_path)
     except Exception as e:
         print(f"Error: {e}")
