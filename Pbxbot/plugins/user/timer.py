a287c0b1e99a1d3148c8a5e87f4fc7f76d6b113c
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-11-23 20:55:07 +0530

    Update timer.py

--- Pbxbot/plugins/user/timer.py
+++ Pbxbot/plugins/user/timer.py
@@ -2,9 +2,10 @@ import os
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from . import *
+from . import HelpMenu, custom_handler, on_message
 
 
-@Client.on_message(filters.self_destruction, group=-6)
+@custom_handler(filters.self_destruction, group=-6)
 async def save_timer_media(client: Client, message: Message):
     try:
         if message.media:
