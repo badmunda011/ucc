66b52ac3c65658c73dfe1b1363dc28958fa9467f
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-12-08 22:27:10 +0530

    Delete Pbxbot/calls.py

--- Pbxbot/calls.py
+++
@@ -1,8 +0,0 @@
-from Pbxbot.core.clients import Pbxbot
-from pytgcalls import PyTgCalls, filters as pytgfl
-from pytgcalls.types import ChatUpdate, Update, GroupCallConfig
-from pytgcalls.types import Call, MediaStream, AudioQuality, VideoQuality
-
-
-call = PyTgCalls(Pbxbot)
-call_config = GroupCallConfig(auto_start=False)
