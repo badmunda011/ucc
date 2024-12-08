8ee7897b079c327e243ff8fba938c1a4369bbdab
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-12-08 21:22:19 +0530

    Update database.py

--- Pbxbot/core/database.py
+++ Pbxbot/core/database.py
@@ -8,7 +8,7 @@ from .config import Config, Symbols
 from .logger import LOGS
 
 mongo_async_cli = _mongo_async_(DATABASE_URL)
-mongodb = mongo_async_cli.badmundadb
+mongodb = mongo_async_cli.badmundaxdb
 
 
 class Database:
