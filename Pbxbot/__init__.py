de5457d0198dca8576108ef4d90eea1e09025d5c
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-12-08 21:29:52 +0530

    Update __init__.py

--- Pbxbot/__init__.py
+++ Pbxbot/__init__.py
@@ -16,10 +16,58 @@ __version__ = {
     "python": python_version(),
 }
 
+#yukki
 
 mongo_async_cli = _mongo_async_(Config.DATABASE_URL)
 db = mongo_async_cli.badmundaxdb
 
+# Served Chats
+
+async def is_served_chat(chat_id: int) -> bool:
+    chat = await chatsdb.find_one({"chat_id": chat_id})
+    if not chat:
+        return False
+    return True
+
+
+async def get_served_chats() -> list:
+    chats_list = []
+    async for chat in chatsdb.find({"chat_id": {"$lt": 0}}):
+        chats_list.append(chat)
+    return chats_list
+
+
+async def add_served_chat(chat_id: int):
+    is_served = await is_served_chat(chat_id)
+    if is_served:
+        return
+    return await chatsdb.insert_one({"chat_id": chat_id})
+
+
+
+# Served Users
+
+async def is_served_user(user_id: int) -> bool:
+    user = await usersdb.find_one({"user_id": user_id})
+    if not user:
+        return False
+    return True
+
+
+async def get_served_users() -> list:
+    users_list = []
+    async for user in usersdb.find({"user_id": {"$gt": 0}}):
+        users_list.append(user)
+    return users_list
+
+
+async def add_served_user(user_id: int):
+    is_served = await is_served_user(user_id)
+    if is_served:
+        return
+    return await usersdb.insert_one({"user_id": user_id})
+
+#yuki
 try:
     if Config.HEROKU_APIKEY is not None and Config.HEROKU_APPNAME is not None:
         HEROKU_APP = heroku3.from_key(Config.HEROKU_APIKEY).apps()[
