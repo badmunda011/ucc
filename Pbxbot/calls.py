736ab33fb5f505095d30863b2e89492d448fa150
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-12-08 22:21:26 +0530

    Update calls.py

--- Pbxbot/calls.py
+++ Pbxbot/calls.py
@@ -1,5 +1,8 @@
+from . import Pbxbot
+from pytgcalls import PyTgCalls, filters as pytgfl
+from pytgcalls.types import ChatUpdate, Update, GroupCallConfig
+from pytgcalls.types import Call, MediaStream, AudioQuality, VideoQuality
 
 
-
-call = PyTgCalls(app)
+call = PyTgCalls(Pbxbot)
 call_config = GroupCallConfig(auto_start=False)
