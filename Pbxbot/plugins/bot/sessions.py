db1a77766f5c2588f81e1db29ce1f3a80ed58789
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-03-04 19:44:34 +0530

    Update sessions.py

--- Pbxbot/plugins/bot/sessions.py
+++ Pbxbot/plugins/bot/sessions.py
@@ -164,6 +164,8 @@ async def session_add(_, message: Message):
 
 BotHelp("Sessions").add(
     "session", "This command is packed with tools to manage userbot sessions."
+    "add", "add session string manually."
 ).info(
     "Session 🚀"
+    "Add"
 ).done()
