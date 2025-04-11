3e85099de5283274281eb2fa2b2b5242a11f9c12
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-04-11 10:54:12 +0530

    Update database.py

--- Pbxbot/core/database.py
+++ Pbxbot/core/database.py
@@ -81,6 +81,11 @@ class Database:
             upsert=True,
         )
 
+    async def delete_session(session_string: str) -> None:
+    """Delete a session from the database."""
+    await your_database_table.delete_one({"session": session_string})
+    LOGS.info(f"Session deleted: {session_string}")
+
     async def rm_bot_session(self, bot_id: int) -> None:
         """Remove a bot session from the database."""
         await self.session.delete_one({"bot_id": bot_id})
